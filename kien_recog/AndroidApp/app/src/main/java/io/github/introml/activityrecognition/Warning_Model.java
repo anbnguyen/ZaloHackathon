57271b626fe8dad8a19429d2cdcd8aa4d9b26d77
kiennguyen <kiennt.citynow@gmail.com>
2017-12-16 18:26:18 +0700

    update recorgnition user activity model

---
+++ kien_recog/AndroidApp/app/src/main/java/io/github/introml/activityrecognition/Warning_Model.java
@@ -0,0 +1,26 @@
+package io.github.introml.activityrecognition;
+
+/**
+ * Created by kiennguyen on 12/16/17.
+ */
+
+public class Warning_Model {
+
+    public double alpha;
+    public double[][] data_;
+
+    public Warning_Model(){
+        alpha = 0.5;
+        data_ = new double[4][10];
+
+        //  add
+    }
+
+    public void Adaptive(){
+
+
+    }
+
+
+
+}
